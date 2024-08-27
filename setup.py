379da6cc95fedb0ac13f7800f2cbbb09fd03723a
Maxwell Davis <maxwell.davis1240@gmail.com>
2024-08-27 18:30:29 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Information Retrieval System (Generative AI Project)",
+    version = "0.0.0",
+    author = "Maxwell Davis",
+    author_email = "maxwell.davis1240@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+    
+)
